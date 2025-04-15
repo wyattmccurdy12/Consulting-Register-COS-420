9f2d9abb2790966943025adad2b4b6885decf6eb
Aubin <mugishaaubin0@gmail.com>
2025-04-15 16:32:03 -0400

    Add GeneralPatient class

---
+++ archive/iteration_2/GeneralPatient.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+import java.util.List;
+
+public class GeneralPatient extends Patient {
+
+    public GeneralPatient(String patientId, Date dateOfBirth, String name, String outPatientNumber,
+                          String healthInsuranceNumber, String nationalIdentificationNumber,
+                          String address, String sex, int age, String motherId, List<Record> records) {
+        super(patientId, dateOfBirth, name, outPatientNumber, healthInsuranceNumber,
+              nationalIdentificationNumber, address, sex, age, motherId, records);
+    }
+
+    // No extra fields yet, but can be added later if needed
+}
