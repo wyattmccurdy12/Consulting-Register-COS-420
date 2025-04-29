d5cddf7508e87e0ec9c98d8838cb56f80efb7a9b
mho9 <mandy.ho@maine.edu>
2025-04-29 15:30:34 -0400

    Remove patientId parameter from GeneralPatient constructor

--- archive/iteration_3/src/GeneralPatient.java
+++ archive/iteration_3/src/GeneralPatient.java
@@ -1,14 +1,13 @@
-package src;
 import java.util.Date;
 import java.util.List;
 
 public class GeneralPatient extends Patient {
 
-    public GeneralPatient(String patientId, Date dateOfBirth, String name, String outPatientNumber,
+    public GeneralPatient(Date dateOfBirth, String name, String outPatientNumber,
                           String healthInsuranceNumber, String nationalIdentificationNumber,
                           String address, String sex, int age, String motherId, List<Record> records) {
-        super(patientId, dateOfBirth, name, outPatientNumber, healthInsuranceNumber,
-              nationalIdentificationNumber, address, sex, age, motherId, records);
+        super(dateOfBirth, name, outPatientNumber, healthInsuranceNumber, nationalIdentificationNumber,
+                address, sex, age, motherId, records);
     }
 
     // No extra fields yet, but can be added later if needed
