68ed9052bd61e038c61b3cbf7479fd60d3181047
mho9 <mandy.ho@maine.edu>
2025-04-29 22:04:26 -0400

    Add childAssessment attribute to Record class

--- archive/iteration_3/src/Record.java
+++ archive/iteration_3/src/Record.java
@@ -1,18 +1,21 @@
-/**
- * @author mandyho
- * abstract record class
- */
 public abstract class Record {
     protected String recordId;
     protected Patient patient;
     protected Clinic clinic;
     protected Visit visit;
+    protected ChildAssessment childAssessment;  // Add a ChildAssessment attribute.
 
     public Record(Patient patient, Clinic clinic, Visit visit) {
         this.recordId = IDGenerator.getInstance().generateRecordId();
         this.patient = patient;
         this.clinic = clinic;
         this.visit = visit;
+
+        if (patient.getAge() < 5) {
+            this.childAssessment = new ChildAssessment("Default Attributes", false, false, false, false, false, false, false, false, false, false, false, false);
+        } else {
+            this.childAssessment = null;
+        }
     }
 
     public String getRecordId() {
@@ -29,5 +32,13 @@ public abstract class Record {
 
     public Visit getVisit() {
         return visit;
+    }
+
+    public ChildAssessment getChildAssessment() {
+        return childAssessment;
+    }
+
+    public void setChildAssessment(ChildAssessment childAssessment) {
+        this.childAssessment = childAssessment;
     }
 }
